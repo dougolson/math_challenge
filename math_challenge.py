bdca169d696a556ea8507c1bc76162f50f1e5cdb
dougolson <mrcolson@gmail.com>
2022-07-08 00:37:30 -0500

    convert game to class and update error checking

--- math_challenge.py
+++ math_challenge.py
@@ -1,3 +1,4 @@
+from ast import Raise
 import random
 import argparse
 import sys
@@ -5,180 +6,180 @@ import time
 import re
 import math
 
-
-def get_random_number(smallest, largest):
-    return random.randint(smallest, largest)
-
-def print_banner(name):
-    banner_string = f"- {name}\'s Math Challenge! -"
-    print("\nWelcome to")
-    spinner = ['+ ','- ','x ','/ ',]
-    for i in range(20):
-        sys.stdout.write(spinner[i%4])
-        sys.stdout.flush()
-        time.sleep(0.05)
-    print("\n")
-    print("-" * len(banner_string))
-    print(f"{banner_string}")
-    print("-" * len(banner_string))
-
-choice_confirmation = {1: "Addition", 2:"Subtraction", 3:"Multiplication", 4:"Division"}
-
-def get_praise():
-    praise = ["Nice!", "Noice!", "Well Done!", "Amazing!", "Fabulous", "Fantastic", "Splendimonious", "Que Bueno!", "You're the Goat!", "Sweet!", "Tasty!"]
-    return praise[get_random_number(0, len(praise) - 1)]
-def get_condolence():
-    condolence = ["Regrettably", "Sadly", "My condolences", "Such a shame", "Oh what a pity", "Not quite", "Doh"]
-    return condolence[get_random_number(0, len(condolence) - 1)]
-
-float_pattern = r'\d*\.\d*'
-
-def validate_and_convert_string_int(string_int):
-    result = None
-    if re.match(float_pattern, string_int):
-        print(f"{get_condolence()}, that is not an integer!")
-        return result
-    try:
-        result = int(string_int)
-        return result
-    except:
-        print("That's not a number!")
-        return result
-
-def addition_problem(smallest, largest):
-    n1 = get_random_number(smallest, largest)
-    n2 = get_random_number(smallest, largest)
-    response = input(f"{n1} + {n2} = ?\n")
-    response = validate_and_convert_string_int(response)
-    if response is not None:
-        if response == n1 + n2:
-            print(f"{get_praise()}")
-        else:
-            print(f"{get_condolence()}, {n1} + {n2} = {n1 + n2}!")    
-    if input("Would you like to try again? y/n\n") == 'y':
-        addition_problem(smallest, largest)
-    else:
-        print("Goodbye!\n")
-        return
-
-def subtraction_problem(smallest, largest):
-    n1 = get_random_number(smallest, largest)
-    n2 = get_random_number(smallest, largest)
-    if n1 < n2:
-        n1, n2 = n2, n1
-    response = input(f"{n1} - {n2} = ?\n")
-    response = validate_and_convert_string_int(response)
-    if response is not None:
-        if response == n1 - n2:
-            print(f"{get_praise()}")
-        else:
-            print(f"{get_condolence()}, {n1} - {n2} = {n1 - n2}!")
-    if input("Would you like to try again? y/n\n") == 'y':
-        subtraction_problem(smallest, largest)
-    else:
-        print("Goodbye!\n")
-        return
-
-def multiplication_problem(smallest, largest):
-    n1 = get_random_number(smallest, largest)
-    n2 = get_random_number(smallest, largest)
-    response = input(f"{n1} x {n2} = ?\n")
-    response = validate_and_convert_string_int(response)
-    if response is not None:
-        if response == n1 * n2:
-            print(f"{get_praise()}")
-        else:
-            print(f"{get_condolence()}, {n1} * {n2} = {n1 * n2}!")
-    if input("Would you like to try again? y/n\n") == 'y':
-        multiplication_problem(smallest, largest)
-    else:
-        print("Goodbye!\n")
-        return
-
-def division_problem(smallest, largest):
-    max_divisor = int(math.sqrt(largest)) + 1
-    candidate_numbers = range(smallest, max_divisor)
-    divisor = random.choice(candidate_numbers)
-    quotient = random.choice(candidate_numbers)
-    print(f"smallest {smallest}")
-    print(f"largest {largest}")
-    print(f"largest // smallest {largest // smallest}")
-    dividend = divisor * quotient
-    print(f"divisor = {divisor}")
-    print(f"quotient  = {quotient}")
-    print(f"dividend  = {dividend}")
-
-    # max_divisor = dividend // quotient
-    # if max_divisor < smallest:
-    #     max_divisor, smallest = smallest, max_divisor
-    # # divisor = n2
-    # divisor = random.choice([x for x in range(smallest, max_divisor + 1)])
-    # print(f"quotient = {quotient}")
-    # dividend = divisor * quotient
-    # print(f"dividend = {dividend}")
-    response = input(f"{dividend} / {divisor} = ?\n")
-    response = validate_and_convert_string_int(response)
-    if response is not None:
-        if response == quotient:
-            print(f"{get_praise()}")
-        else:
-            print(f"{get_condolence()}, {dividend} / {divisor} = {quotient}!")
-    if input("Would you like to try again? y/n\n") == 'y':
-        division_problem(smallest, largest)
-    else:
-        setup('')
-        return   
-
-def quit_program(dummy_1, dummy_2):
-    print("Goodbye!\n")
-    sys.exit()
-
-function_choice = {1: addition_problem, 2: subtraction_problem, 3: multiplication_problem, 4: division_problem, 5:quit_program}
-
-def setup(name):
-    if first_run:
-        print_banner(name)
-        first_run = False  
-    print("What do you want to practice?")
-    # print()
-    choice = input('''
-    1 - Addition
-    2 - Subtraction
-    3 - Multiplication
-    4 - Division
-    5 - Quit
-    ''')
-    try: 
-        choice = int(choice)
-        print(f"{choice_confirmation[choice]} - great choice!")
-    except:
-        print("Sorry - I didn't understand that")
-    smallest_number = input("What's the smallest number you want to use?\n")
-    try:
-        smallest_number =int(smallest_number)
-    except:
-        print("that doesn't seem to be a number!")
-    largest_number = input("What's the largest number you want to use?\n")
-    try:
-        largest_number =int(largest_number)
-    except:
-        print("that doesn't seem to be a number!")
-
-    print(f"Great! Let's do some {choice_confirmation[choice]} problems with numbers between {smallest_number} and {largest_number}...")
-    answer = function_choice[choice](smallest_number, largest_number)
-
-
+class MathChallenge():
     
+    def __init__(self):
+        self.first_run = True
+        self.choice_confirmation = {1: "Addition", 2:"Subtraction", 3:"Multiplication", 4:"Division", 5:"Quit"}
+        self.float_pattern = r'(-|\+)?\d*\.\d*'
+        self.function_choice = {1: self.addition_problem, 2: self.subtraction_problem, 3: self.multiplication_problem, 4: self.division_problem, 5:self.quit_program}
+
+    def get_random_number(self, smallest, largest):
+        return random.randint(smallest, largest)
+
+    def print_banner(self, name):
+        banner_string = f"- {name}\'s Math Challenge! -"
+        print("\nWelcome to")
+        spinner = ['+ ','- ','x ','/ ',]
+        for i in range(20):
+            sys.stdout.write(spinner[i%4])
+            sys.stdout.flush()
+            time.sleep(0.05)
+        print("\n")
+        print("-" * len(banner_string))
+        print(f"{banner_string}")
+        print("-" * len(banner_string))
+
+    def get_praise(self, ):
+        praise = ["Nice!", "Noice!", "Well Done!", "Amazing!", "Fabulous", "Fantastic", "Splendimonious", "Que Bueno!", "You're the Goat!", "Sweet!", "Tasty!"]
+        return praise[self.get_random_number(0, len(praise) - 1)]
     
+    def get_condolence(self, ):
+        condolence = ["Regrettably", "Sadly", "My condolences", "Such a shame", "Oh what a pity", "Not quite", "Doh"]
+        return condolence[self.get_random_number(0, len(condolence) - 1)]
 
-# def main():
+    def validate_and_convert_string_int(self, string_int):
+        result = None
+        if re.match(self.float_pattern, string_int):
+            print(f"{self.get_condolence()}, number must be a positive integer!")
+            return result
+        try:
+            result = int(string_int)
+            if result < 1:
+                print(f"{self.get_condolence()}, number must be a positive integer!")
+                return None
+            return result
+        except:
+            print("That's not a number!")
+            return result
 
+    def addition_problem(self, smallest, largest):
+        n1 = self.get_random_number(smallest, largest)
+        n2 = self.get_random_number(smallest, largest)
+        response = input(f"{n1} + {n2} = ?\n")
+        response = self.validate_and_convert_string_int(response)
+        if response is not None:
+            if response == n1 + n2:
+                print(f"{self.get_praise()}")
+            else:
+                print(f"{self.get_condolence()}, {n1} + {n2} = {n1 + n2}!")    
+        if input("Would you like to try again? y/n\n") == 'y':
+            self.addition_problem(smallest, largest)
+        else:
+            self.setup('')
+            return 
 
+    def subtraction_problem(self, smallest, largest):
+        n1 = self.get_random_number(smallest, largest)
+        n2 = self.get_random_number(smallest, largest)
+        if n1 < n2:
+            n1, n2 = n2, n1
+        response = input(f"{n1} - {n2} = ?\n")
+        response = self.validate_and_convert_string_int(response)
+        if response is not None:
+            if response == n1 - n2:
+                print(f"{self.get_praise()}")
+            else:
+                print(f"{self.get_condolence()}, {n1} - {n2} = {n1 - n2}!")
+        if input("Would you like to try again? y/n\n") == 'y':
+            self.subtraction_problem(smallest, largest)
+        else:
+            self.setup('')
+            return 
+
+    def multiplication_problem(self, smallest, largest):
+        n1 = self.get_random_number(smallest, largest)
+        n2 = self.get_random_number(smallest, largest)
+        response = input(f"{n1} x {n2} = ?\n")
+        response = self.validate_and_convert_string_int(response)
+        if response is not None:
+            if response == n1 * n2:
+                print(f"{self.get_praise()}")
+            else:
+                print(f"{self.get_condolence()}, {n1} * {n2} = {n1 * n2}!")
+        if input("Would you like to try again? y/n\n") == 'y':
+            self.multiplication_problem(smallest, largest)
+        else:
+            self.setup('')
+            return 
+
+    def division_problem(self, smallest, largest):
+        max_divisor = int(math.sqrt(largest)) + 1
+        candidate_numbers = range(smallest, max_divisor)
+        divisor = random.choice(candidate_numbers)
+        quotient = random.choice(candidate_numbers)
+        dividend = divisor * quotient
+        response = input(f"{dividend} / {divisor} = ?\n")
+        response = self.validate_and_convert_string_int(response)
+        if response is not None:
+            if response == quotient:
+                print(f"{self.get_praise()}")
+            else:
+                print(f"{self.get_condolence()}, {dividend} / {divisor} = {quotient}!")
+        if input("Would you like to try again? y/n\n") == 'y':
+            self.division_problem(smallest, largest)
+        else:
+            self.setup('')
+            return   
+
+    def quit_program(self):
+        sys.exit("Goodbye!")
+
+    def get_number(self, designation):
+        number = input(f"What's the {designation} number you want to use?\n")
+        number = self.validate_and_convert_string_int(number)
+        return number
+
+    def setup(self, name):
+        if self.first_run:
+            self.print_banner(name)
+            self.first_run = False  
+        print("What do you want to practice?")
+        choice = input('''
+        1 - Addition
+        2 - Subtraction
+        3 - Multiplication
+        4 - Division
+        5 - Quit
+        ''')
+        try: 
+            choice = self.validate_and_convert_string_int(choice)
+            if choice < 5:
+                print(f"{self.choice_confirmation[choice]} - great choice!")
+            if choice > 5:
+                print("Sorry - I didn't understand that")
+                self.setup('')
+        except:
+            print("Sorry - I didn't understand that")
+            self.setup('')
+        if choice == 5:
+            self.quit_program()
+            return
+        # smallest_number = input("What's the smallest number you want to use?\n")
+        smallest_number = 0
+        while (smallest_number is None or smallest_number < 1):
+            smallest_number = self.get_number("smallest")
+            print(f"smallest_number = {smallest_number}")
+        largest_number = smallest_number
+        while (largest_number is None or largest_number <= smallest_number):
+            largest_number = self.get_number("largest")
+        # try:
+        # except:
+        #     print("that doesn't seem to be a number!")
+        # largest_number = input("What's the largest number you want to use?\n")
+        # try:
+        #     largest_number =self.validate_and_convert_string_int(largest_number)
+        # except:
+        #     print("that doesn't seem to be a number!")
+
+        print(f"Great! Let's do some {self.choice_confirmation[choice]} problems with numbers between {smallest_number} and {largest_number}...")
+        self.function_choice[choice](smallest_number, largest_number)
+        return
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("name", type=str, default="Mr. Doo-Doo Head")
+    parser.add_argument("name", type=str, nargs = '?', default="Anonymous Doo-Doo Head")
     args = parser.parse_args()
     player_name = args.name
-    first_run = True
-    setup(player_name)
+    game = MathChallenge()
+    game.setup(player_name)
